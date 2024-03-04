5fe10729a75cc050171c4d0a5033aaa468d8c727
Yossi Weinberger <137178593+yossi-weinberger@users.noreply.github.com>
2024-03-04 13:13:23 +0200

    fixed credentials

--- mongo_config.py
+++ mongo_config.py
@@ -1,14 +1,13 @@
+from credentials import mongo
 from pymongo.mongo_client import MongoClient
-# from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://ayw100:cG0o1wgVTRUSxuJ5@cluster0.r8gajka.mongodb.net/?retryWrites=true&w=majority"
-
+uri = f"mongodb+srv://{mongo["username"]}:{mongo["password"]}@cluster0.r8gajka.mongodb.net/?retryWrites=true&w=majority"
 # Create a new client and connect to the server
 client = MongoClient(uri)
 
 # Send a ping to confirm a successful connection
-# try:
-#     client.admin.command('ping')
-#     print("Pinged your deployment. You successfully connected to MongoDB!")
-# except Exception as e:
-#     print(e)
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
